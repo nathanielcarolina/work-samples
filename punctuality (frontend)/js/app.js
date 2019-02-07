b34952dee27a4b7349b1c6492af478db08755dd9
lucedeswede <nathanielcarolina@gmail.com>
2019-02-07 16:40:16 +0800

    Populate initial rostered start and finish data

--- punctuality (frontend)/js/app.js
+++ punctuality (frontend)/js/app.js
@@ -1,7 +1,12 @@
-let tableDay = document.getElementById('table-day');
-let tableBody = document.getElementById('table-body');
+let tableDay = document.getElementById("table-day");
+let tableBody = document.getElementById("table-body");
 let tableData;
+let tableContents = "";
 let dates;
+let starts;
+let actualStarts;
+let finishes;
+let actualFinishes;
 
 
 
@@ -9,7 +14,21 @@ let dates;
 fetch("http://localhost:4567/rosters/2013-08-15/2013-09-15").then((response) => { response.text().then((object) =>
   { tableData = JSON.parse(object);
     console.log(tableData);
-    dates = tableData.map(i => i.date);
-    console.log(dates);
-    dates.forEach(date => { tableBody.innerHTML += '<tr><th scope="row" id="table-day">'+date+'</th></tr>'; });
+    for (let i = 0; i < tableData.length; i++) {
+      tableContents += "<tr><th scope='row'>" + tableData[i].date + "</th>" +
+        "<td>" + tableData[i].start + "</td>" +
+        "<td>" + "" + "</td>" +
+        "<td>" + tableData[i].finish + "</td>" +
+        "<td>" + "" + "</td>" +
+        "</tr>"
+    }
+    tableBody.innerHTML = tableContents;
+
+    // dates = tableData.map(i => i.date);
+    // starts = tableData.map(i => i.start);
+    // console.log(dates);
+    // dates.forEach(date => { tableBody.innerHTML +=
+    //   "<tr><th scope='row' id='table-day'>" + date + "</th></tr>"
+    //   "<td></td>"
+    //   ; });
   }) });
